73023050884157a4aee6683db10a03fbca456a58
narendra1378 <narendra000mahajan@gmail.com>
2024-09-11 14:58:11 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Box from "./Component/Box1";
 
 function App() {
+  const [dataSource, setDataSource] = useState(Array.from({ length: 10 }));
+  const [hasMore, setHasMore] = useState(true);
+  const fetchMoreData = () => {
+    setTimeout(() => {
+      setDataSource(dataSource.concat(Array.from({ length: 20 })));
+    }, 500);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InfiniteScroll
+        dataLength={dataSource.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={<p>Loading...</p>}
+      >
+        {dataSource.map((item, index) => {
+          // <div style={style}>Thus is div #{index + 1}InfiniteScroll</div>
+          return <Box />;
+        })}
+      </InfiniteScroll>
     </div>
   );
 }
---
+++ src/Component/Box.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Box = () => {
+  return (
+    <div
+      style={{ border: "1px solid blue", margin: 12, padding: 8, height: 100 }}
+    >
+      This is a Box component
+    </div>
+  );
+};
+
+export default Box;
